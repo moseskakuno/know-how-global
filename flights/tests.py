51376f234e0eb4cb1e3f1e2e922e75dff2af1c0b
Moses Ochieng Akuno <moseskakuno@gmail.com>
2026-02-02 16:25:36 +0100

    corrected tests.py

--- flights/tests.py
+++ flights/tests.py
@@ -58,7 +58,7 @@ class FlightTestCase(TestCase):
         max_id = Flight.objects.all().aggregate(Max("id"))["id__max"] 
                 
         c = Client()
-        response = c.get(f"/flights/{max_id+1}")
+        response = c.get(f"/flights/{max_id}")
         self.assertEqual(response.status_code, 404)
         
     def test_flight_page_passengers(self):
